f5f16dcd742806df67e5d0f6643b04495c1e2ffc
Haifa <haifanaj@gmail.com>
2021-03-02 10:24:08 -0600

    circle marker

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,23 +5,36 @@
 var myMap = L.map("mapid", {
     center: [37.0902, -95.7129],
     zoom: 5
-  });
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
-    
-geoUrl= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+}).addTo(myMap);
 
-d3.json(geoUrl,function(data){
-    L.geoJson(data).addTo(myMap);
-});
+geoUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+d3.json(geoUrl,function(data) {
+    L.geoJson(data,{
+    // L.geoJson(data).addTo(myMap);
+    pointToLayer:function(feature,latlng){
+        return L.circleMarker(latlng,geojsonMarkerOptions)}
+    }).addTo(myMap)
+})
+
 
 
