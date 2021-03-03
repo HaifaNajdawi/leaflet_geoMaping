5cb6fb918634af6df0016371a676c36819405c29
Haifa <haifanaj@gmail.com>
2021-03-03 15:57:16 -0600

    legand

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -92,6 +92,27 @@ console.log(d3.min(mag))
 
 
     }).addTo(myMap)
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML += "<h4>Earthquake Depth</h4>";
+    div.innerHTML += '<i style="background: green"></i><span>-10-10</span><br>';
+    div.innerHTML += '<i style="background: yellow"></i><span>10-30</span><br>';
+    div.innerHTML += '<i style="background: orange"></i><span>30-50</span><br>';
+    div.innerHTML += '<i style="background: brown"></i><span>50-70</span><br>';
+    div.innerHTML += '<i style="background: purple"></i><span>70-90</span><br>';
+    div.innerHTML += '<i style="background: red"></i><span>+90</span><br>';
+
+    div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Grænse</span><br>';
+    
+    
+
+    return div;
+    };
+
+    legend.addTo(map);
+
 })
 
 
