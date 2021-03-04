16e8a795bf9411ae145e3365bcb3fce544b6839f
Haifa <haifanaj@gmail.com>
2021-03-04 11:28:20 -0600

    legand added

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,32 +1,80 @@
-// Creating map object
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-var myMap = L.map("mapid", {
-    center: [37.0902, -95.7129],
-    zoom: 5
-});
+function createMap(tect, earthquake) {
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-baseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-}).addTo(myMap);
+    // Adding a tile layer (the background map image) to our map
+    // We use the addTo method to add objects to our map
+    lightLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    })
 
-grayLayer= L.tileLayer.grayscale('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 14,
-   minZoom: 2,
-   className :"leaflet-tile-pane"
-}).addTo(myMap);
+    // create gray scale layer that one was for old computers and put class with style for background 
+    // grayLayer = L.tileLayer.grayscale('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
+    //     maxZoom: 18,
+    //     minZoom: 2,
+    //     className: "leaflet-tile-pane"
+    // })
 
+    //using google satlite layer
+    satelliteLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 18,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    })
+
+    baseLayer = {
+        "Satellite": satelliteLayer,
+        // "GrayScale":grayLayer,
+        "Outdoors": lightLayer
+    }
+    overlayMap = {
+        "Earthquake": earthquake,
+        "Tectonic Plates":tect
+    }
+
+    // Creating map object
+    // Creating our initial map object
+    // We set the longitude, latitude, and the starting zoom level
+    // This gets inserted into the div with an id of 'mapid'
+    var myMap = L.map("mapid", {
+        center: [37.0902, -95.7129],
+        zoom: 5,
+        layers: [lightLayer, earthquake]
+    });
+
+    L.control.layers(baseLayer, overlayMap, {
+        collapsed: false
+    }).addTo(myMap)
+
+    d3.json(geoUrl, function (data) {
+        var legend = L.control({ position: "bottomright" });
+    
+        legend.onAdd = function (map) {
+            var div = L.DomUtil.create("div", "legend");
+            div.innerHTML += "<h4>Earthquake Depth</h4>";
+            div.innerHTML += '<i style="background: green"></i><span>(-10)-10</span><br>';
+            div.innerHTML += '<i style="background: yellow"></i><span>10-30</span><br>';
+            div.innerHTML += '<i style="background: orange"></i><span>30-50</span><br>';
+            div.innerHTML += '<i style="background: brown"></i><span>50-70</span><br>';
+            div.innerHTML += '<i style="background: purple"></i><span>70-90</span><br>';
+            div.innerHTML += '<i style="background: red"></i><span>+90</span><br>';
+    
+    
+            return div;
+        };
+    
+        legend.addTo(myMap);
+    })
+    
+}
+
+// earthquake json file for last 7 days 
 geoUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// function for  using in style later put switch true because condition return true or false 
 function geojsonMarkerColor(depth) {
 
     switch (true) {
@@ -72,52 +120,45 @@ function geojsonMarkerRadius(mag) {
 };
 
 
+// using d3 lib to put the data in the function 
+function createCircleMarker() {
+    let circles=new L.FeatureGroup();
+    d3.json(geoUrl, function (data) {
+        // loop to know max and min magnitude 
+        mag = []
+        for (var i = 0; i < data.features.length; i++) {
 
-d3.json(geoUrl, function (data) {
-
-    mag = []
-    for (var i = 0; i < data.features.length; i++) {
-
-        mag.push(data.features[i].properties.mag)
-    }
-    console.log(d3.max(mag))
-    console.log(d3.min(mag))
-
-    L.geoJson(data, {
-
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: 8,
-                fillColor: geojsonMarkerColor(feature.geometry.coordinates[2]),
-                color: "white",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8,
-                radius: geojsonMarkerRadius(feature.properties.mag)
-            }).bindPopup(`<h3> Location: ${feature.properties.place}</h3><br><h4>Magnitude: ${feature.properties.mag}</h4><br><h4>Depth Of Earthquake: ${feature.geometry.coordinates[2]}</h4>`)
+            mag.push(data.features[i].properties.mag)
         }
+        console.log(d3.max(mag))
+        console.log(d3.min(mag))
+        // using leaflet lib to get json data coordainates 
+        L.geoJson(data, {
+            // using key name  before function 
+            pointToLayer: function (feature, latlng) {
+                // to create circle marker 
+                circle = L.circleMarker(latlng, {
+                    // style for data 
+                    radius: 8,
+                    fillColor: geojsonMarkerColor(feature.geometry.coordinates[2]),
+                    color: "white",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8,
+                    radius: geojsonMarkerRadius(feature.properties.mag)
+                    // put tootips once click on circles 
+                }).bindPopup(`<h3> Location: ${feature.properties.place}</h3><br><h4>Magnitude: ${feature.properties.mag}</h4><br><h4>Depth Of Earthquake: ${feature.geometry.coordinates[2]}</h4>`)
+                return circle;
+            }
+        }).addTo(circles);
+        
+    });
+    console.log(circles);
+    return circles;
+}
+d3.json
 
 
-    }).addTo(myMap)
-    var legend = L.control({ position: "bottomright" });
-
-    legend.onAdd = function (map) {
-        var div = L.DomUtil.create("div", "legend");
-        div.innerHTML += "<h4>Earthquake Depth</h4>";
-        div.innerHTML += '<i style="background: green"></i><span>-10-10</span><br>';
-        div.innerHTML += '<i style="background: yellow"></i><span>10-30</span><br>';
-        div.innerHTML += '<i style="background: orange"></i><span>30-50</span><br>';
-        div.innerHTML += '<i style="background: brown"></i><span>50-70</span><br>';
-        div.innerHTML += '<i style="background: purple"></i><span>70-90</span><br>';
-        div.innerHTML += '<i style="background: red"></i><span>+90</span><br>';
-
-
-        return div;
-    };
-
-    legend.addTo(myMap);
-
-})
 platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
 
 var mapStyle = {
@@ -125,13 +166,17 @@ var mapStyle = {
     fillOpacity: 0,
     weight: 3
 };
+function createTectonicMarker() {
+    tectonic=new L.FeatureGroup();
+    d3.json(platesUrl, function (tect) {
+        console.log("plates", tect)
 
-d3.json(platesUrl, function (data) {
-    markers = L.markerClusterGroup();
-    console.log("plates", data)
-    markers.addLayer(L.geoJson(data, { style: mapStyle }))
-    myMap.addLayer(markers)
-})
+        L.geoJson(tect, { style: mapStyle }).addTo(tectonic)
 
+    })
+    return tectonic;
+}
+
+createMap(createTectonicMarker(), createCircleMarker())
 
 
