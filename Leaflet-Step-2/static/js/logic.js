d4c13d33410085cf1b8a553aa7cc4b50754ec7a3
Haifa <haifanaj@gmail.com>
2021-03-05 11:50:51 -0600

    read me

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -18,9 +18,13 @@ function createMap(tect, earthquake) {
         minZoom: 2,
     })
     //using google satlite layer
-    satelliteLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    satelliteLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
         maxZoom: 18,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+        zoomOffset: -1,
+        id: "mapbox/satellite-v9",
+        accessToken: API_KEY
     })
 
     baseLayer = {
